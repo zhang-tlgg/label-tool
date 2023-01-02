3d3b9c3431095bc3bcb2bf8d2c2b31fe2e648001
Tianle Zhang <2446588658@qq.com>
2023-01-02 12:52:27 +0800

    '修复16位灰度图显示的问题'

--- mainwindow.py
+++ mainwindow.py
@@ -2,7 +2,7 @@ import sys, os
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QGraphicsScene
 from PyQt5.QtGui import QImage, QPixmap
-from skimage import io
+import skimage
 import numpy as np
 
 import ui.Ui_mainwindow as Ui_mainwindow
@@ -23,49 +23,44 @@ class MainWindow(QMainWindow):
         file_name, _ = QFileDialog.getOpenFileName(self, "Open file", self.recent_path, "Image files (*.jpg *.gif *.png *.jpeg *.tif)")
         self.recent_path, _ = os.path.split(file_name)
         print(file_name)
-        self.show_img(file_name)
+        self.load_img(file_name)
 
-    def set_z(self, value):
-        self.ui.horizontalSlider.setValue(value)
-        self.ui.spinBox.setValue(value)
-        img = np.array(self.img[value])
-        img = np.uint8(255 * (img - self.img_min) / (self.img_max - self.img_min))
+    def set_value(self, value):
+        if(self.sender() == self.ui.horizontalSlider or self.sender() == self.ui.spinBox):
+            self.ui.horizontalSlider.setValue(value)
+            self.ui.spinBox.setValue(value)
+        elif(self.sender() == self.ui.horizontalSlider_2 or self.sender() == self.ui.spinBox_2):
+            self.ui.horizontalSlider_2.setValue(value)
+            self.ui.spinBox_2.setValue(value)
+        self.show_img()
+
+    # 成员函数
+    def load_img(self, file_name):
+        # 读入图片
+        self.img = skimage.io.imread(file_name)
+        self.show_img()
+        # 设置部件
+        self.ui.horizontalSlider.setMaximum(self.img.shape[0] - 1)
+        self.ui.horizontalSlider.valueChanged.connect(self.set_value)
+        self.ui.spinBox.setMaximum(self.img.shape[0] - 1)
+        self.ui.spinBox.valueChanged.connect(self.set_value)
+        self.ui.horizontalSlider_2.valueChanged.connect(self.set_value)
+        self.ui.spinBox_2.valueChanged.connect(self.set_value)
+        
+    def show_img(self):
+        img = np.array(self.img[self.ui.horizontalSlider.value()])
         h = img.shape[0]
         w = img.shape[1]
-        frame = QImage(img, w, h, w, QImage.Format_Grayscale8)
+        if(img.dtype == 'uint16'):
+            img = np.where(img > self.ui.horizontalSlider_2.value(), 65535, img)
+            frame = QImage(img, w, h, QImage.Format_Grayscale16)
+        else:
+            img = np.where(img > self.ui.horizontalSlider_2.value(), 255, img)
+            frame = QImage(img, w, h, w, QImage.Format_Grayscale8)
         pix = QPixmap.fromImage(frame).scaled(500, 500)
         scene = QGraphicsScene()
         scene.addPixmap(pix)
         self.ui.graphicsView.setScene(scene)
-
-    # 成员函数
-    def show_img(self, file_name):
-        # 读入图片
-        self.img = io.imread(file_name)  # img = img_as_ubyte(io.imread(file_name))
-        self.img_min = np.min(self.img)
-        self.img_max = np.max(self.img)
-        band = self.img.shape[0]
-        img = np.array(self.img[0])
-        img = np.uint8(255 * (img - self.img_min) / (self.img_max - self.img_min))
-        print(img.shape, img.dtype)
-        h = img.shape[0]
-        w = img.shape[1]
-        frame = QImage(img, w, h, w, QImage.Format_Grayscale8)
-        pix = QPixmap.fromImage(frame).scaled(480, 480)
-        scene = QGraphicsScene()
-        scene.addPixmap(pix)
-        
-        # 构建部件
-        self.ui.graphicsView.setScene(scene)
-        self.ui.graphicsView.show()
-        self.ui.horizontalSlider.setMaximum(band - 1)
-        self.ui.horizontalSlider.setValue(0)
-        self.ui.horizontalSlider.valueChanged.connect(self.set_z)
-        self.ui.horizontalSlider.show()
-        self.ui.spinBox.setMaximum(band - 1)
-        self.ui.spinBox.setValue(0)
-        self.ui.spinBox.valueChanged.connect(self.set_z)
-        self.ui.spinBox.show()
     
 
 if __name__ =="__main__":
